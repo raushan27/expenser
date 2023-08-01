d1b18f1ed5640e4d98253cc7b9bd4d90a97f0648
Raushan <raushanvadshah@gmail.com>
2023-08-01 18:40:12 +0530

    Added better visibility for mobile devices

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,20 +5,18 @@ function Header() {
     <div>
       <div className="my-header">
         <img
-          style={{ height: "65px", margin: "10px" }}
-          className=""
+          className="logo"
           src={require("../../images/logo.png")}
           alt="App Logo"
         />
         <img
-          style={{ height: "65px", margin: "10px" }}
-          className=""
+          className="logo-name"
           src={require("../../images/logo-name.png")}
           alt="App Name"
         />
         <a href="https://github.com/raushan27/expenser">
           <img
-            style={{ height: "55px", margin: "10px" }}
+            className="github"
             src={require("../../images/github.gif")}
             alt="git-logo"
           ></img>
